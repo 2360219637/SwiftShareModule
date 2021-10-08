6000a6fc019c526724999a8cebb41f87d0a59369
chenzhichao <chenzhichao.vip@bytedance.com>
2021-10-08 16:47:41 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-SwiftShareModule_Tests/Pods-SwiftShareModule_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftShareModule_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftShareModule_Tests
+@end
---
+++ Example/Pods/Target Support Files/SwiftShareModule/SwiftShareModule-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftShareModule : NSObject
+@end
+@implementation PodsDummy_SwiftShareModule
+@end
